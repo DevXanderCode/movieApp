63a7b58b24ebcabcba03228ee6e5f46b880b6555
Nweke Chinedu Alex <nwekealexchinedu@gmail.com>
2021-12-26 22:51:10 +0100

    added axios and created the rest client folder

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
 
 const App = () => {
+  const logger = 'test the console logging';
+  console.log(logger);
   return (
     <SafeAreaView style={{flex: 1}}>
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
