0cb331c4d36740d35445e64d1df6bc7e8eaaaeba
Nweke Chinedu Alex <nwekealexchinedu@gmail.com>
2021-12-26 23:53:36 +0100

    worked on getting the popular movies from the api

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import * as React from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
+import axios from 'axios';
+
+const getPopularMovies = async () => {
+  const res = await axios.get(
+    'https://api.themoviedb.org/3/movie/popular?api_key=06e864e959ef3841f17579868f21a540',
+  );
+  return res.data.results;
+};
 
 const App = () => {
-  const logger = 'test the console logging';
-  console.log(logger);
+  const [movie, setMovie] = React.useState('');
+
+  React.useEffect(() => {
+    getPopularMovies().then(movie => {
+      setMovie(movie[0]);
+    });
+  }, []);
   return (
     <SafeAreaView style={{flex: 1}}>
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text>Hello World!</Text>
+        <Text>Movie Name: {movie.original_title}</Text>
+        <Text>Language: {movie.original_language}</Text>
+        <Text>Release Date: {movie.release_date}</Text>
       </View>
     </SafeAreaView>
   );
