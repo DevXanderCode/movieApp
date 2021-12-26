7e22fa150a662773d55415822907bca77fdaa780
Nweke Chinedu Alex <nwekealexchinedu@gmail.com>
2021-12-27 00:49:03 +0100

    started working on the home component

---
+++ screens/Home.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import {Text, View, SafeAreaView} from 'react-native';
+import {getPopularMovies} from '../services/services';
+
+const Home = () => {
+  const [movie, setMovie] = React.useState('');
+  const [error, setError] = React.useState(false);
+
+  React.useEffect(() => {
+    getPopularMovies()
+      .then(movie => {
+        setMovie(movie[0]);
+        setError(false);
+      })
+      .catch(err => {
+        setError(err);
+      });
+  }, []);
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>Movie Name: {movie.original_title}</Text>
+        <Text>Language: {movie.original_language}</Text>
+        <Text>Release Date: {movie.release_date}</Text>
+        {error && (
+          <Text style={{color: 'red'}}>
+            errors in the server: {error.message}
+          </Text>
+        )}
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
