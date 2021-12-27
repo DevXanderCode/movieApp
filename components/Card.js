af6e620f38ab13d3da7fb70a34cd72080517a868
Nweke Chinedu Alex <nwekealexchinedu@gmail.com>
2021-12-27 08:00:22 +0100

    added placeholder image to the list component

--- components/Card.js
+++ components/Card.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import {TouchableOpacity, StyleSheet} from 'react-native';
+import {TouchableOpacity, StyleSheet, Text} from 'react-native';
 import FastImage from 'react-native-fast-image';
+
+const placeholderImg = require('../assets/images/image-placeholder.png');
 
 class Card extends React.PureComponent {
   render() {
@@ -8,10 +10,18 @@ class Card extends React.PureComponent {
     return (
       <TouchableOpacity onPress={() => {}} style={styles?.container}>
         <FastImage
-          source={{uri: `https://image.tmdb.org/t/p/w500${item?.poster_path}`}}
+          source={
+            item?.poster_path
+              ? {uri: `https://image.tmdb.org/t/p/w500${item?.poster_path}`}
+              : placeholderImg
+          }
           style={styles?.imgStyle}
           resiezeMode="cover"
+          placeholder={placeholderImg}
         />
+        {!item?.poster_path && (
+          <Text style={styles?.movieName}>{item?.title}</Text>
+        )}
       </TouchableOpacity>
     );
   }
@@ -21,11 +31,18 @@ const styles = StyleSheet.create({
   container: {
     padding: 5,
     position: 'relative',
+    height: 200,
+    alignItems: 'center',
   },
   imgStyle: {
     width: 120,
     height: 200,
     borderRadius: 20,
+  },
+  movieName: {
+    position: 'absolute',
+    textAlign: 'center',
+    top: 10,
   },
 });
 
