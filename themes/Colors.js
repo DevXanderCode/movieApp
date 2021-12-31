3259e97696d5b522619f0ec29d8f3103fc1b15e8
Nweke Chinedu Alex <nwekealexchinedu@gmail.com>
2021-12-31 09:40:30 +0100

    created a list of colors in the theme

---
+++ themes/Colors.js
@@ -0,0 +1,10 @@
+const Colors = {
+  backgroundColor: '#1E1E1E',
+  black: '#000',
+  white: '#fff',
+  lightGray: '#EDEDED',
+  primary: '#4481FC',
+  danger: '#F5365c',
+};
+
+export default Colors;
